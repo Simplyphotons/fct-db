0cfe8deb1bc97b4274b843f2d220210075a96f83
Dennis <20250762@studentmail.ul.ie>
2024-04-11 15:05:28 +0100

    Remove extra lines in the script

--- scripts/patches/patch-1.1.sql
+++ scripts/patches/patch-1.1.sql
@@ -2,7 +2,5 @@
 
 --changeset  dennis.kolomiyets:add_col_users
 --comment:  added bool to users
-
-
 alter table if exists users
 add column if not exists is_supervisor BOOLEAN DEFAULT false;
--- scripts/patches/patch-1.2.sql
+++ scripts/patches/patch-1.2.sql
@@ -2,7 +2,6 @@
 
 --changeset  dennis.kolomiyets:user_test_data context:test_data
 --comment: Adding test data for user bools
-
 UPDATE users SET is_supervisor = 'true' where id = 'auth0|660e9f0660dbd55b5bbd9a16';
 UPDATE users SET is_supervisor = 'true' where id = 'auth0|661136c8a009dbb7b90e3d4a';
 UPDATE users SET is_supervisor = 'true' where id = 'auth0|6611382b9fbd80a7b657b715';
