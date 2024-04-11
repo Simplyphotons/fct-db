0605f0f6e2cec38a7991e907bce7f7a8e7981bb6
Dennis <20250762@studentmail.ul.ie>
2024-04-11 14:43:28 +0100

    re added is_supervisor bool - fix

--- scripts/patches/patch-1.1.sql
+++ scripts/patches/patch-1.1.sql
@@ -1,2 +1,8 @@
+--liquibase formatted sql
+
+--changeset  dennis.kolomiyets:add_col_users
+--comment:  added bool to users
+
+
 alter table if exists users
 add column if not exists is_supervisor BOOLEAN DEFAULT false;
